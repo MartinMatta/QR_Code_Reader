ac5e8709985b37389906442af46bb795a58a99a5
MartinMatta <martinmatta355@gmail.com>
2021-03-16 20:35:23 +0100

    edit ShowActivity.kt

---
+++ app/src/main/java/com/example/qrcode/ShowActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qrcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
