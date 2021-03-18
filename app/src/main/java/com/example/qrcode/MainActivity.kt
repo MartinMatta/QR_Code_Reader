a70279fd2d8b49bba735dc354e23ea86e23e4e77
MartinMatta <martinmatta355@gmail.com>
2021-03-18 20:16:39 +0100

    add qr scan function

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -2,17 +2,40 @@ package com.example.qrcode
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-import android.widget.Toast
-
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        scanQRCode()
+
         //startActivityForResult(Intent(this@MainActivity, QrCodeActivity::class.java),101)
 
+    }
+
+    private fun scanQRCode(){
+        val integrator = IntentIntegrator(this).apply {
+            captureActivity = CaptureActivity::class.java
+            setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+            set
+            setPrompt("")
+        }
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
