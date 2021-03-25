665e348d740503927c331b041801cc443b8f4528
MartinMatta <martinmatta355@gmail.com>
2021-03-25 20:08:08 +0100

    add (exitDialog, onBackPressed, startFragment) function in MainActivity.kt

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.qrcode
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.internal.ContextUtils.getActivity
+import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            val fragment = ScanFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.frame, fragment)
-                .commit()
+            startFragment(ScanFragment())
         }
 
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigation)
@@ -27,29 +28,55 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.scan -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame, ScanFragment())
-                        .commit()
+                    startFragment(ScanFragment())
                     true
                 }
                 R.id.generate -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame, GenerateFragment())
-                        .commit()
+                    startFragment(GenerateFragment())
                     true
                 }
                 R.id.history -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame, HistoryFragment())
-                        .commit()
+                    startFragment(HistoryFragment())
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            return
+        } else {
+            exitDialog()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
+    private fun exitDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.exit_dialog, null)
+        val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+
+        val btnCancel = mDialogView.findViewById<Button>(R.id.btnCancel)
+        val btnOk = mDialogView.findViewById<Button>(R.id.btnOk)
+
+        val  mAlertDialog = mBuilder.show()
+
+        btnCancel.setOnClickListener {
+            mAlertDialog.dismiss()
+        }
+
+        btnOk.setOnClickListener {
+            exitProcess(1)
+        }
+    }
+
+    private fun startFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame, fragment)
+                .commit()
     }
 
 }
