9ad8b4391ad49bbb08c03a4b26a1fdb1e3e1bec4
MartinMatta <martinmatta355@gmail.com>
2021-04-02 20:55:00 +0200

    edit ScanCodeFragment.kt

--- app/src/main/java/com/example/qrcode/Fragments/ScanCodeFragment.kt
+++ app/src/main/java/com/example/qrcode/Fragments/ScanCodeFragment.kt
@@ -5,24 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
 import com.example.qrcode.R
 
 class ScanCodeFragment : Fragment() {
-
     private lateinit var codeScanner: CodeScanner
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-        //activity?.title = "Scan"
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_scan_code, container, false)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+            }
+        }
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
 
-        return inflater.inflate(R.layout.fragment_scan_code, container, false)
+    override fun onResume() {
+        codeScanner.startPreview()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
