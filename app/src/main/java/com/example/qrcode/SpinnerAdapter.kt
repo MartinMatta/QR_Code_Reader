8a1a1084129620af18cc6552c9c23c3d3396b9da
MartinMatta <martinmatta355@gmail.com>
2021-04-18 19:05:27 +0200

    edit SpinnerAdapter.kt

--- app/src/main/java/com/example/qrcode/SpinnerAdapter.kt
+++ app/src/main/java/com/example/qrcode/SpinnerAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.qrcode
 
 import android.content.Context
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 
-class CustomSpinnerAdapter(ctx: Context, countries: ArrayList<CountryData>) : ArrayAdapter<CountryData>(ctx, 0, countries) {
+class CustomSpinnerAdapter(ctx: Context, countries: ArrayList<SpinnerData>) : ArrayAdapter<SpinnerData>(ctx, 0, countries) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         return createItemView(position, convertView, parent);
@@ -20,17 +19,11 @@ class CustomSpinnerAdapter(ctx: Context, countries: ArrayList<CountryData>) : Ar
     fun createItemView(position: Int, recycledView: View?, parent: ViewGroup):View {
         val country = getItem(position)
 
-        val view = recycledView ?: LayoutInflater.from(context).inflate(
+        return recycledView ?: LayoutInflater.from(context).inflate(
                 R.layout.spinner_row,
                 parent,
                 false
         )
-
-        country?.let {
-            view..setImageResource(country.flag)
-            view.textViewCountryName.text = country.countryName
-        }
-        return view
     }
 
 
