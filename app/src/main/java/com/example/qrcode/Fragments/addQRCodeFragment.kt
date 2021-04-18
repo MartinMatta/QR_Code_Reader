faa8b53ee613f3db0bfdb800a856f2987d0daa6d
MartinMatta <martinmatta355@gmail.com>
2021-04-18 20:11:50 +0200

    create generateQRCode(

--- app/src/main/java/com/example/qrcode/Fragments/addQRCodeFragment.kt
+++ app/src/main/java/com/example/qrcode/Fragments/addQRCodeFragment.kt
@@ -1,17 +1,28 @@
 package com.example.qrcode.Fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.ListView
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.fragment.app.Fragment
 import com.example.qrcode.R
+import com.google.zxing.WriterException
+
 
 class addQRCodeFragment : Fragment() {
+
+    private var bitmap: Bitmap? = null
+    private var qrgEncoder: QRGEncoder? = null
+    private var qrText: EditText? = null
+    private var qrImage: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +34,8 @@ class addQRCodeFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_add_qr_code, container, false)
 
         val spinner: Spinner = view.findViewById(R.id.spinner)
+        qrText = view.findViewById(R.id.qrText)
+        qrImage = view.findViewById(R.id.qrImage)
 
         //val saveButton: ImageButton = view.findViewById(R.id.buttonSave)
         //val shareButton: ImageButton = view.findViewById(R.id.buttonShare)
@@ -44,5 +57,28 @@ class addQRCodeFragment : Fragment() {
         spinner.adapter = adapter
 
         return view
+    }
+
+    fun generateQRCode() {
+
+        var type = "text"
+
+        when (type) {
+            "text" -> type =  QRGContents.Type.TEXT
+            "contact" -> type =  QRGContents.Type.CONTACT
+            "sms" -> type =  QRGContents.Type.SMS
+            "phone" -> type =  QRGContents.Type.PHONE
+            "location" -> type =  QRGContents.Type.LOCATION
+            "email" -> type =  QRGContents.Type.EMAIL
+            else -> print("otherwise")
+        }
+        qrgEncoder = QRGEncoder(qrText?.text.toString(), null, type, 5)
+
+        try {
+            bitmap = qrgEncoder!!.encodeAsBitmap()
+            qrImage?.setImageBitmap(bitmap)
+        } catch (e: WriterException) {
+            Log.e("Tag", e.toString())
+        }
     }
 }
