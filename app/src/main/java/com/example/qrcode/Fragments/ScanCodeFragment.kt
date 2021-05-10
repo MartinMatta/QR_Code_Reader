1bcbe9f8545996b29efa688de8674cd781e90827
MartinMatta <martinmatta355@gmail.com>
2021-05-10 13:46:30 +0200

    checkpoint

--- app/src/main/java/com/example/qrcode/Fragments/ScanCodeFragment.kt
+++ app/src/main/java/com/example/qrcode/Fragments/ScanCodeFragment.kt
@@ -1,14 +1,16 @@
 package com.example.qrcode.Fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.CodeScannerView
-import com.budiyev.android.codescanner.DecodeCallback
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
 import com.example.qrcode.R
 
 class ScanCodeFragment : Fragment() {
@@ -23,14 +25,41 @@ class ScanCodeFragment : Fragment() {
         val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
         val activity = requireActivity()
         codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.TWO_DIMENSIONAL_FORMATS // all_format
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
         codeScanner.decodeCallback = DecodeCallback {
             activity.runOnUiThread {
-                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                Toast.makeText(requireContext(), "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            activity.runOnUiThread {
+                Toast.makeText(requireContext(), "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG).show()
             }
         }
         scannerView.setOnClickListener {
             codeScanner.startPreview()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(::codeScanner.isInitialized) {
+            codeScanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if(::codeScanner.isInitialized) {
+            codeScanner?.releaseResources()
+        }
+        super.onPause()
     }
 
 }
