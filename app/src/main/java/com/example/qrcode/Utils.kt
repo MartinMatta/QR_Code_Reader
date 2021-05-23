f0673ab9492a0e24877af71b2bc1555818e531f7
MartinMatta <martinmatta355@gmail.com>
2021-05-23 17:04:43 +0200

    clear Utils.kt

--- app/src/main/java/com/example/qrcode/Utils.kt
+++ app/src/main/java/com/example/qrcode/Utils.kt
@@ -21,16 +21,7 @@ class Utils {
         }
     }
 
-    //fun openUrl(url: String) {
-        //val uri: Uri = Uri.parse(url)
-        //startActivity(Intent(Intent.ACTION_VIEW, uri))
-        //startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-    //}
-
-
     private fun String.isValidUrl(): Boolean = Patterns.WEB_URL.matcher(this).matches()
-    //private fun String.isValidEmail(): Boolean = Patterns.EMAIL_ADDRESS.matcher(this).matches()
-    //private fun String.isValidPhone(): Boolean = Patterns.PHONE.matcher(this).matches()
 
     private fun String.isValidSMS(): Boolean {
         if ("SMSTO:" in  this) {
