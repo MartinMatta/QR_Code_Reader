dc51a429623f1fe47af2a93dfa54ed22112fd06f
MartinMatta <martinmatta355@gmail.com>
2021-06-13 20:51:01 +0200

    edit Constants.kt

--- app/src/main/java/com/example/qrcode/Constants.kt
+++ app/src/main/java/com/example/qrcode/Constants.kt
@@ -1,4 +1,12 @@
 package com.example.qrcode
 
 class Constants {
+
+    private val images: MutableMap<String?, Int?> = HashMap()
+
+    fun init() {
+       images["wifi"] = R.drawable.wifi
+    }
+
+
 }
